2f8217e02e293a67773b6767a4396b4bb29e369b
PRATHAM MORE <pratham02more20@gmail.com>
2023-05-04 10:01:01 +0530

    leaderboard limited to top 3

--- client/pages/dataquest/leaderboard.js
+++ client/pages/dataquest/leaderboard.js
@@ -79,7 +79,7 @@ const Leader = () => {
           >
             Leaderboard (Top 3) {senior ? "( TE-BE )" : "( FE-SE )"}
           </chakra.h1>
-          {submissions?.length > 100 ? (
+          {submissions?.length > 3 ? (
             <Leaderboard submissions={submissions} senior={senior} />
           ) : (
             <chakra.h3
