7000b4c31bc3e002a8cecc7f45872cb394f98932
PRATHAM MORE <pratham02more20@gmail.com>
2023-05-04 09:59:59 +0530

    changes

--- client/pages/dataquest/leaderboard.js
+++ client/pages/dataquest/leaderboard.js
@@ -77,7 +77,7 @@ const Leader = () => {
             color={textColor}
             pb={"40px"}
           >
-            Leaderboard {senior ? "( TE-BE )" : "( FE-SE )"}
+            Leaderboard (Top 3) {senior ? "( TE-BE )" : "( FE-SE )"}
           </chakra.h1>
           {submissions?.length > 100 ? (
             <Leaderboard submissions={submissions} senior={senior} />
